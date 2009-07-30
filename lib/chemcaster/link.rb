95c62e49b4f536eac1b7b7362d08e4fcd66f2110
Rich Apodaca <rapodaca@metamolecular.com>
2009-07-30 15:11:43 -0700

    media type used by link

--- lib/chemcaster/link.rb
+++ lib/chemcaster/link.rb
@@ -3,21 +3,22 @@ module Chemcaster
     def initialize atts={}
       @client = RestClient::Resource.new atts['uri']
       @media_class = MediaType.locate atts['media_type']
+      @media_type = atts['media_type']
     end
     
     def get
-      response = decode @client.get(:accept => @media_class.mime_type)
+      response = decode @client.get(:accept => @media_type)
       @media_class.new response
     end
     
     def put representation
       response = decode @client.put(encode(representation),
-        :accept => @media_class.mime_type, :content_type => @media_class.mime_type)
+        :accept => @media_type, :content_type => @media_type)
       @media_class.new response
     end
     
     def delete
-      response = decode @client.delete(:accept => @media_class.mime_type)
+      response = decode @client.delete(:accept => @media__type)
       @media_class.new response
     end
     
