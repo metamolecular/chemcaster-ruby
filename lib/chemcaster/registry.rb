ad8bb7227485b0a1f9047ffe66a4f50dd86f6a4e
Rich Apodaca <rapodaca@metamolecular.com>
2009-08-24 09:28:44 -0700

    added deletable attribute to Registry

--- lib/chemcaster/registry.rb
+++ lib/chemcaster/registry.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../chemcaster/item'
 
 module Chemcaster
   class Registry < Item
-    attributes :name
+    attributes :name, :deletable
     resources :queries, :structures
   end
 end
